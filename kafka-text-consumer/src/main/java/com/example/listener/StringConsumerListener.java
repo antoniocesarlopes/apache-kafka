8451b8be8b4ff2f5df3e4a61dea1409ee144f885
antoniocesar <alopes.java@gmail.com>
2023-01-23 09:54:56 -0300

    implementando Listener Customizado

--- kafka-text-consumer/src/main/java/com/example/listener/StringConsumerListener.java
+++ kafka-text-consumer/src/main/java/com/example/listener/StringConsumerListener.java
@@ -3,16 +3,37 @@ package com.example.listener;
 import java.util.logging.Logger;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
+
+import com.example.listener.custom.StringConsumercCustomListener;
 
 @Component
 public class StringConsumerListener {
-	
+
 	private Logger logger = Logger.getLogger(StringConsumerListener.class.getName());
-	
-	@KafkaListener(groupId = "str-group", topics = "str-topic", containerFactory = "stringContainerFactory" )
-	public void listener(String message) {
-		logger.info(String.format("Recieved message %s", message));
+
+	@KafkaListener(groupId = "str-group-1", 
+			topicPartitions = { 
+					@TopicPartition(topic = "str-topic", partitions = {"0"})}, 
+			containerFactory = "stringContainerFactory" )
+	public void listenerTest1(String message) {
+		logger.info(String.format("TEST 1 ::: Recieved message %s", message));
+	}
+
+	@KafkaListener(groupId = "str-group-1", 
+			topicPartitions = { 
+					@TopicPartition(topic = "str-topic", partitions = {"1"})}, 
+			containerFactory = "stringContainerFactory" )
+	public void listenerTest2(String message) {
+		logger.info(String.format("TEST 2 ::: Recieved message %s", message));
+	}
+
+	//Escuta as duas partições
+	//@KafkaListener(groupId = "str-group-2", topics = "str-topic", containerFactory = "stringContainerFactory" )
+	@StringConsumercCustomListener(groupId = "str-group-2")
+	public void listenerTest3(String message) {
+		logger.info(String.format("TEST 3 ::: Recieved message %s", message));
 	}
 
 }
